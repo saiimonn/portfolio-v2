68188dfcfa7a867f3b5d5ff73e8bbdfd99eb1b93
saiimonn <gementizasgg08@gmail.com>
2026-01-04 21:50:19 +0800

    feat: dependencies installed and added providers

---
+++ app/provider.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import type { ReactNode } from "react"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { useState } from "react"
+
+export default function AppProvider({ children }: { children: ReactNode }) {
+  const [queryClient] = useState(() => new QueryClient());
+  
+  return (
+    <QueryClientProvider client = {queryClient}>
+      {children}
+    </QueryClientProvider>
+  )
+}
