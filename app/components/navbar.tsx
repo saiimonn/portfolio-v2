4e364b98e946e66b993fff22acdf7ce7c79a5288
Sai <gementizasgg08@gmail.com>
2026-01-15 13:50:05 +0800

    switch to pc

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,33 +1,32 @@
 import Link from "next/link";
 
 const Nav = () => {
+  const navLinks = [
+    { name: "Home", href: "/" },
+    { name: "About", href: "/about" },
+    { name: "Work", href: "/projects" },
+    { name: "Contact", href: "/contact" },
+  ];
+
   return (
-    <div className = "w-full flex justify-center space-x-8 text-white font-bold p-1">
-      <div>
-        <Link href = "/">
-          <h1>Home</h1>
-        </Link>
+    <nav className="fixed top-6 left-1/2 -translate-x-1/2 z-50">
+      <div className="flex items-center gap-6 px-4 py-3 bg-black/10 backdrop-blur-md border border-black/20 rounded-2xl shadow-lg">
+        {/* Navigation Links */}
+        <ul className="flex items-center gap-5">
+          {navLinks.map((link) => (
+            <li key={link.name}>
+              <Link
+                href={link.href}
+                className="text-sm font-medium text-white/70 hover:text-white transition-colors duration-200"
+              >
+                {link.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
       </div>
-      
-      <div>
-        <Link href = "/about">
-          <h1>About</h1>
-        </Link>
-      </div>
-      
-      <div>
-        <Link href = "/projects">
-          <h1>Work</h1>
-        </Link>
-      </div>
-      
-      <div>
-        <Link href = "/contact">
-          <h1>Contact</h1>
-        </Link>
-      </div>
-    </div>
-  )
-}
+    </nav>
+  );
+};
 
 export default Nav;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local"
 import "./globals.css";
 import AppProvider from "./provider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const generalSans = localFont({
+  src: '../public/fonts/GeneralSans-Regular.woff2',
+  display: 'swap',
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${generalSans.className} antialiased`}
       >
         <AppProvider>
           {children}
