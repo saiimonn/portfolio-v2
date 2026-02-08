75f3873328214984d547331813a7e923350dd59e
saiimonn <gementizasgg08@gmail.com>
2026-02-09 02:00:28 +0800

    chore: added more photos for ml-hub

--- app/features/landing/projects.ts
+++ app/features/landing/projects.ts
@@ -4,7 +4,7 @@ export const projects = [
   {
     number: "01",
     name: "ML Hub",
-    img: ["/images/projects/ml-hub/ml-hub-landing.png", "/images/projects/ml-hub/ml-hub-analytical.png"],
+    img: ["/images/projects/ml-hub/ml-hub-landing.png", "/images/projects/ml-hub/ml-hub-analytical.png", "/images/projects/ml-hub/ml-hub-color-analyzer.png", "/images/projects/ml-hub/ml-hub-edge-detector.png"],
     shortDesc: "A unified web platform for testing and showcasing my machine learning models.",
     longDesc: "A personal web platform where I bring together all of my machine learning models in one place. It lets users interact with and test different ML-powered features through a clean and intuitive interface. Built as both a learning space and a showcase, ML-Hub highlights how machine learning models can be deployed and used in real-world web applications.",
     stack: ["NextJS", "Tailwindcss", "FastAPI", "OpenCV"]
