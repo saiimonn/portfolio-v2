ff22d1a76c795587e864f815f9fdd951534e0b95
saiimonn <gementizasgg08@gmail.com>
2026-05-02 14:15:16 +0800

    feat(projects): add nitpicker in projects

--- app/features/landing/projects.ts
+++ app/features/landing/projects.ts
@@ -4,7 +4,12 @@ export const projects = [
   {
     number: "01",
     name: "ML Hub",
-    img: ["/images/projects/ml-hub/ml-hub-landing.png", "/images/projects/ml-hub/ml-hub-analytical.png", "/images/projects/ml-hub/ml-hub-color-analyzer.png", "/images/projects/ml-hub/ml-hub-edge-detector.png"],
+    img: [
+      "/images/projects/ml-hub/ml-hub-landing.png",
+      "/images/projects/ml-hub/ml-hub-analytical.png",
+      "/images/projects/ml-hub/ml-hub-color-analyzer.png",
+      "/images/projects/ml-hub/ml-hub-edge-detector.png"
+    ],
     shortDesc: "A unified web platform for testing and showcasing my machine learning models.",
     longDesc: "A personal web platform where I bring together all of my machine learning models in one place. It lets users interact with and test different ML-powered features through a clean and intuitive interface. Built as both a learning space and a showcase, ML-Hub highlights how machine learning models can be deployed and used in real-world web applications.",
     stack: ["NextJS", "Tailwindcss", "FastAPI", "OpenCV"],
@@ -15,7 +20,12 @@ export const projects = [
   {
     number: "02",
     name: "Val Residences",
-    img: ["/images/projects/val-residences/AboutUs1.png", "/images/projects/val-residences/AboutUs2.png", "/images/projects/val-residences/Dashboard2.png", "/images/projects/val-residences/Listings.png"],
+    img: [
+      "/images/projects/val-residences/AboutUs1.png",
+      "/images/projects/val-residences/AboutUs2.png",
+      "/images/projects/val-residences/Dashboard2.png",
+      "/images/projects/val-residences/Listings.png"
+    ],
     shortDesc: "A full-stack apartment management system for my classmates' family business.",
     longDesc: "A comprehensive multi-tenant rental management system that connects landlords, tenants, and prospective tenants and manages the entire rental life cycle.",
     stack: ["ReactJS", "Tailwindcss", "InertiaJS", "Laravel", "MySQL"],
@@ -25,10 +35,31 @@ export const projects = [
   {
     number: "03",
     name: "Tipsy Trails",
-    img: ["/images/projects/tipsy-trails/brands_carousel.png", "/images/projects/tipsy-trails/footer.png", "/images/projects/tipsy-trails/our_story.png", "/images/projects/tipsy-trails/signature_cocktails.png"],
-    shortDesc: "A frontend-focused CMS project for showcasing",
-    longDesc: "A simple static web application for the Tipsy Trails Mobile Bar to showcase their menu, story, etc.",
+    img: [
+      "/images/projects/tipsy-trails/brands_carousel.png",
+      "/images/projects/tipsy-trails/footer.png",
+      "/images/projects/tipsy-trails/our_story.png",
+      "/images/projects/tipsy-trails/signature_cocktails.png"
+    ],
+    shortDesc: "Frontend-driven CMS website for a mobile bar brand",
+    longDesc: "Developed a responsive, CMS-powered website for Tipsy Trails Mobile Bar to showcase their menu, brand story, and services. Integrated Payload CMS and Supabase for content management, enabling dynamic updates without redeployment. Focused on clean UI/UX, smooth animations, and scalable component architecture.",
     stack: ["NextJS", "Tailwindcss", "Supabase", "PayloadCMS"],
     repoLink: "https://github.com/lucerocris/tipsytrails"
+  },
+
+  {
+    number: "04",
+    name: "Nitpicker",
+    img: [
+      "/images/projects/nitpicker/landing.png",
+      "/images/projects/nitpicker/mock_exam.png",
+      "/images/projects/nitpicker/mock_exam_prep.png",
+      "/images/projects/nitpicker/prev_exams.png"
+    ],
+    shortDesc: "Interactive mock exam platform for PhilNITS preparation.",
+    longDesc: "Helped build an interactive mock exam platform to help students prepare for the PhilNITS certification. Implemented timed exams, question navigation, and preparation flows with a focus on usability and performance. Utilized animations to enhance user engagement and simulate real testing environments.",
+    stack: ["NextJS", "Tailwindcss", "Framer Motion"],
+    repoLink: "https://github.com/noni-was-taken/NitPicker",
+    siteLink: "https://nitpicker.dcism.org"
   }
 ]
